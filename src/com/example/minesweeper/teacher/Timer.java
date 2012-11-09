bbf1309c8d998543b6798bba3ca3c49a9822a055
Bryan Ngo <bngo92@gmail.com>
2012-11-08 23:51:12 -0800

    Fixed timer bug

--- src/com/example/minesweeper/teacher/Timer.java
+++ src/com/example/minesweeper/teacher/Timer.java
@@ -4,18 +4,20 @@ import android.os.Handler;
 import android.widget.TextView;
 
 public class Timer extends Handler {
+	final int UPDATE_TIME = 10;
+	
 	private TextView textView;
-	private int mSeconds;
+	private int mMillis;
 	
 	Timer(TextView tv) {
 		textView = tv;
-		mSeconds = 0;
+		mMillis = 0;
 	}
 
 	public void startTimer() {
 		resetTimer();
 		removeCallbacks(updateTimer);
-		postDelayed(updateTimer, 1000);
+		postDelayed(updateTimer, UPDATE_TIME);
 	}
 	
 	public void stopTimer() {
@@ -23,15 +25,16 @@ public class Timer extends Handler {
 	}
 	
 	private Runnable updateTimer = new Runnable() {
-		public void run() {
-			mSeconds++;
-			textView.setText("" + mSeconds);
-			postDelayed(updateTimer, 1000);
+		public void run() {	
+			mMillis += UPDATE_TIME;
+			textView.setText(String.format("%.2f", mMillis/1000.));
+			postDelayed(updateTimer, UPDATE_TIME);
 		}
 	};
 	
 	public void resetTimer() {
-		mSeconds = 0;
-		textView.setText("0");
+		removeCallbacks(updateTimer);
+		mMillis = 0;
+		textView.setText("0.00");
 	}
 }
